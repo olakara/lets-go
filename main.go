b54927d9af2ad2f861a6d352f8438a032041177d
Abdel Raoof <olakara@gmail.com>
2025-05-17 11:32:30 +0400

    Refactor snippet view handler to extract and validate snippet ID from URL

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -12,7 +13,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL.Path)
-	w.Write([]byte("Display a specific snippet"))
+	// Extract the snippet ID from the URL
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	msg := "Display a specific snippet with ID: " + strconv.Itoa(id)
+	w.Write([]byte(msg))
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -23,8 +31,8 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 func main() {
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/home", home)
-	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/home/{$}", home)
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 	log.Println("Starting server on :8080")
 	err := http.ListenAndServe(":8080", mux)
