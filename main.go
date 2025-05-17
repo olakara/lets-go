06896b9b5c2af58ecb80fef60e3a09cfec7b0689
Abdel Raoof <olakara@gmail.com>
2025-05-17 15:10:18 +0400

    Add common headers to response in handlers

--- main.go
+++ main.go
@@ -6,12 +6,19 @@ import (
 	"strconv"
 )
 
+func addCommonHeaders(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("X-Server", "Go")
+}
+
 func home(w http.ResponseWriter, r *http.Request) {
+	addCommonHeaders(w)
 	log.Println(r.URL.Path)
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
+	addCommonHeaders(w)
 	log.Println(r.URL.Path)
 	// Extract the snippet ID from the URL
 	id, err := strconv.Atoi(r.PathValue("id"))
@@ -25,11 +32,14 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL.Path)
+	addCommonHeaders(w)
 	w.Write([]byte("Create a new snippet"))
 }
 
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL.Path)
+	addCommonHeaders(w)
+	w.WriteHeader(http.StatusCreated)
 	// Simulate creating a new snippet
 	w.Write([]byte("Snippet created"))
 }
