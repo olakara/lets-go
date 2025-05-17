68913b57e87617cb1b432f972f59fa1e87711485
Abdel Raoof <olakara@gmail.com>
2025-05-17 13:21:27 +0400

    Add snippetCreatePost handler and update route mappings for snippet creation

--- main.go
+++ main.go
@@ -28,12 +28,19 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Create a new snippet"))
 }
 
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL.Path)
+	// Simulate creating a new snippet
+	w.Write([]byte("Snippet created"))
+}
+
 func main() {
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/home/{$}", home)
-	mux.HandleFunc("/snippet/view/{id}", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("GET /home/{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 	log.Println("Starting server on :8080")
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
