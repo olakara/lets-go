30ce24b461dcb767a3eeefd96e25632265d59897
Abdel Raoof <olakara@gmail.com>
2025-05-17 23:11:44 +0400

    Refactor main function to use config struct for address and static directory

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,23 +6,32 @@ import (
 	"net/http"
 )
 
+type config struct {
+	addr string
+	staticDir string
+}
+
+
 func main() {
 
-	addr := flag.String("addr", ":8080", "HTTP network address")
+	var cfg config
+
+	flag.StringVar(&cfg.addr,"addr", ":8080", "HTTP network address")
+	flag.StringVar(&cfg.staticDir,"staticDir", "./ui/static/", "Path to static files")
 	flag.Parse()
-	
+
 
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("GET /home/{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 	
-	log.Println("Starting server on " + *addr)
-	err := http.ListenAndServe(*addr, mux)
+	log.Println("Starting server on " + cfg.addr)
+	err := http.ListenAndServe(cfg.addr, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
