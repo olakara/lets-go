b4ff6bdfdc5eb9ef6ee13b0c62bdeaaaa5a346d6
Abdel Raoof <olakara@gmail.com>
2025-05-17 11:15:41 +0400

    Replace helloHandler with home handler and update route mappings for snippet views and creation

--- main.go
+++ main.go
@@ -5,16 +5,24 @@ import (
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("Hello, World!"))
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet"))
 }
 
 func main() {
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", helloHandler)
+	mux.HandleFunc("/home", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 	log.Println("Starting server on :8080")
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
