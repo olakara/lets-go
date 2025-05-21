56cbd97c21114caadf54a565620c74a1a9915d47
Abdel Raoof <olakara@gmail.com>
2025-05-21 21:24:55 +0400

    Refactor snippet creation routes to specify HTTP methods.
    Updated route definitions for snippet creation to explicitly differentiate between GET and POST methods. This improves clarity and aligns with RESTful API practices.

--- cmd/web/handlers/common.go
+++ cmd/web/handlers/common.go
@@ -43,8 +43,8 @@ func (app *Application) Routes() *http.ServeMux {
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/home/", app.homeHandler)
 	mux.HandleFunc("/snippet/view/{id}", app.snippetViewHandler)
-	mux.HandleFunc("/snippet/create", app.snippetCreateHandler)
-	mux.HandleFunc("/snippet/create", app.snippetCreatePostHandler)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreateHandler)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePostHandler)
 
 	return mux
 }
